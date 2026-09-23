d65d131a8b5d37cb8afb379ae1712dac04849e54
sriram@12 <sriramulu.sriram33@gmail.com>
2026-09-23 14:11:25 +0530

    schema level validations and api level validations

--- src/app.js
+++ src/app.js
@@ -1,25 +1,75 @@
 let express=require("express");
 let app=express();
+let validator=require("validator")
 const {connectDb}=require("./config/database")
+app.use(express.json());
 let User=require("./model/User")
-app.post("/signUp",async(req,res)=>{
 
-   let userInstance=new User({
-      firstName:"siri",
-      lastName:"chintalapalli",
-      age:23,
-      Gender:"FeMale"
-
+app.get("/user",async(req,res)=>{
+  let email=req.body.emailId
+  
+  let user=await User.findOne({emailId:email})    
+  res.send(user);
 })
+app.get("/feed",async(req,res)=>{
+  
+  
+  let user=await User.find({})     
+  res.send(user);
+})
+
+
+
+app.post("/signUp",async(req,res)=>{
+  
+  
+  
+     let userInstance=new User(
+         req.body
+)  
    try{
     await userInstance.save();
-   res.send("sucessfully signUp");
-   }
-   catch(err){
-     res.send(err.message)
-   }
+    res.send("sucessfully signUp");
+    }
+    catch(err){
+      res.send(err.message)
+    }
 
+})
 
+app.delete("/delete",async(req,res)=>{
+ let userid=req.body.userId;
+ console.log(userid)
+let user= await User.findByIdAndDelete(userid)
+ res.send("suceessfully deleted")
+
+})
+app.patch("/update/:userId",async(req,res)=>{
+  let userid=req.params.userId
+  let data=req.body
+  
+   try{
+    let AllowedUpdates=["firstName","lastName","age","gender","photoUrl","about","passWord"]
+  let isallowUpdates=Object.keys(data).every((k)=>{
+     return  AllowedUpdates.includes(k)
+  })
+ 
+  if(!isallowUpdates){
+     throw new Error("You can only update allowed fields")
+  }
+
+  
+
+  let user=await User.findByIdAndUpdate({_id:userid},data,
+    {runValidators:true}
+  );
+  res.send("updated suceesfully");
+ }
+ catch(err){
+   res.status(400).send(err.message)
+ }
+
+  
 
 })
 
--- src/model/User.js
+++ src/model/User.js
@@ -1,19 +1,85 @@
 let mongoose=require("mongoose")
+let validator=require("validator")
 let userSchema=mongoose.Schema({
 firstName:{
-    type:String
+    type:String,
+    required:true,
+    minLength:4,
+    maxLength:50,
+    validate(value){
+      let isvalidFirstName=validator.isAlpha(value)
+      if(!isvalidFirstName){
+        throw new Error("FirstName should be only letters")
+      }
+    }
+
 },
 lastName:{
-    type:String
+    type:String,
+    validate(value){
+      let isvalidlastName=validator.isAlpha(value)
+      if(!isvalidlastName){
+        throw new Error("lastName should be only letters")
+      }
+    }
+
 },
 emailId:{
-    type:String
+    type:String,
+    required:true,
+    unique:true,
+    trim:true,
+    lowercase:true,
+    validate(value){
+      let isValidEmail=validator.isEmail(value)
+      if(!isValidEmail){
+         throw new Error("invalid email address"+value)
+      } 
+    }
+
 },
 age:{
-    type:Number
+    type:Number,
+    min:18,
+    max:100
+},
+passWord:{
+  type:String,
+  required:true,
+  validate(value){
+   let isvalidpassword=validator.isStrongPassword(value);
+   if(!isvalidpassword){
+    throw new Error("Password must be at least 8 characters and include an uppercase letter, a lowercase letter, a number, and a symbol")
+   }
+
+  }
+
+  
+
+},
+about:{
+    type:String,
+    trim:true,
+    default:"This is the about the user"
+},
+photoUrl:{
+    type:String,
+    default:"https://static.vecteezy.com/system/resources/previews/036/594/092/non_2x/man-empty-avatar-photo-placeholder-for-social-networks-resumes-forums-and-dating-sites-male-and-female-no-photo-images-for-unfilled-user-profile-free-vector.jpg"
 },
 
 
+gender:{
+    type:String,
+    required:true,
+    lowercase:true,
+    validate(value){
+    if(!["male","female","others"].includes(value)){
+         throw new Error("gender data is not valid")
+          
+    }
+
+    }
+}
 
 
 
@@ -21,6 +87,8 @@ age:{
 
 
 
-})
+
+
+},{ timestamps: true })
 
 module.exports=mongoose.model("User",userSchema);
